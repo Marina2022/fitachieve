7ad80b08438874a521443325260aa82f7681467a
Marina2022 <marusiiiia@yandex.ru>
2024-05-06 20:09:07 +0300

    height

--- components/userPage/Notes.tsx
+++ components/userPage/Notes.tsx
@@ -30,7 +30,7 @@ const Notes = () => {
 
     return (
         <>            
-        <textarea spellCheck="false" className="mt-5 h-[150px] w-full p-4 border mb-5" value={notes}
+        <textarea spellCheck="false" className="mt-5 h-[150px] w-full p-4 border mb-5 max-md:h-[120px]" value={notes}
                   onChange={(e) => setNotes(e.target.value)}>            
             {
                 notes
--- components/userPage/PlanRange.tsx
+++ components/userPage/PlanRange.tsx
@@ -11,7 +11,7 @@ const PlanRange = ({minSquareQuantity, workedSquares}: {minSquareQuantity:number
     console.log({fulfilledPercent})
 
     return (
-        <div className="h-[25px] border-amber-300 bg-amber-50 mb-[30px] rounded-md relative">
+        <div className="h-[25px] border-amber-300 bg-amber-50 mb-[30px] rounded-md relative max-md:mb-[20px]">
             <div style={{width: `${fulfilledPercent}%`}} className="rounded-md t-0 l-0 h-[100%] bg-amber-300"></div>           
             
         </div>
